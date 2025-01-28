d221f7a6e66040d53061ebba37d86b1f7bd984eb
Chris Martinez <chmartinez2014@gmail.com>
2025-01-28 15:16:46 -0600

    Added opening comments and started sudo code

---
+++ main.py
@@ -0,0 +1,19 @@
+# Title: Fermat’s Last Theorem
+# File name: main.py
+# Necessary external files:  N/a
+# Eexternal files your program creates plus an explanation for each file: N/a
+# Programmer Names: Chris Martinez, Zachariah Kennedy
+# Programmer Emails: chrisrmartinez@lewisu.edu, zachariahckennedy@lewisu.edu
+# Course number and section number:SP25-CPSC-44000-LT1
+# Date submitted: 
+# What the program does: 
+# Resources used: 
+
+
+# SUDO CODE 
+# Start
+# EQUATION: x^n + y^n = z^n
+# CONSTRAINTS: n is a natural number greater than 2, k is greater than or equal to 10
+# INPUT: Ask the user for n (the power to use in the equation) 
+# INPUT: Ask the user of k (which limits the range of x and y possibilities to test). 
+# 
